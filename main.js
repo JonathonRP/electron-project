9f4e3e0050397965702ab55d5bf5c964da01985e
Reese Perry <jreesep@gmail.com>
2017-08-01 22:12:22 -0500

    improved

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
-app.on('ready', function(){
-    var mainWindow = new BrowserWindow({
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({
         width: 1920,
         height: 1080,
         frame: false
     })
-    mainWindow.webContents.openDevTools()
-})
+    
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    win.webContents.openDevTools()
+}
+
+app.on('ready', createWindow);
