d99e74a6379c341b5d523bcb783dda44fa46d3a5
Reese Perry <jreesep@gmail.com>
2017-08-15 19:44:30 -0500

    testing

--- app/menuHandler.js
+++ app/menuHandler.js
@@ -1,9 +1,10 @@
 var python = require('python.node')
 const {remote} = require('electron')
-const {browser} = python.import('browser')
+var brython = require('brython')
+var browser = python.import('browser')
 
 var win = remote.getCurrentWindow();
 
-browser.document['minimize'].click( function(){
+browser.document['minimize'].bind( click, function(){
     win.minimize()
 });
