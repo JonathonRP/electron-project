3d150aca19ec88e27217aa413e60b8d0be3ab5e0
Jonathon Reese Perry <jreesep@gmail.com>
2019-02-14 18:35:09 -0600

    updated style and look

--- brython-electron/app/py/documentHandler.py
+++ brython-electron/app/py/documentHandler.py
@@ -1,10 +1,2 @@
 from browser import document
 
-def test(event):
-    document["welcome"].text = "Hello World"
-
-def reset(event):
-    document["welcome"].text = "welcome"
-
-document["speak"].bind("click", test)
-document["reset"].bind("click", reset)
