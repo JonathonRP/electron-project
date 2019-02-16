1aec14facab018baf7d0b1e16a9c0d31cca4fa69
Jonathon Reese Perry <jreesep@gmail.com>
2019-02-16 00:50:12 -0600

    buttoning-up the style

--- brython-electron/app/py/documentHandler.py
+++ brython-electron/app/py/documentHandler.py
@@ -1,2 +1,17 @@
-from browser import document
+from browser import document, window
 
+def addItem(event):
+    if event.keyCode == 13:
+        document.select(".main")[0].classList.remove("hidden")
+        document.select(".footer")[0].classList.remove("hidden")
+        document.select(".new-todo")[0].value = ""
+
+def creator(event):
+    window.open('https://github.com/JonathonRP', 'electron', 'frame=true')
+
+def TodoMVC(event):
+    window.open('http://todomvc.com', 'electron', 'frame=true')
+
+document.select(".new-todo")[0].bind('keypress', addItem)
+document["creator"].bind('click', creator)
+document["TodoMVC"].bind('click', TodoMVC)
